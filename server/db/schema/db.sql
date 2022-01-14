5159e76213aa3747fcc2d1ad3bc3e35188317ad8
Amohamed96 <amohamed.rg@gmail.com>
2022-01-14 18:34:32 -0500

    database setup

--- server/db/schema/db.sql
+++ server/db/schema/db.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE user_ratings (
@@ -10,3 +11,4 @@ CREATE TABLE user_ratings (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0
   );
+
