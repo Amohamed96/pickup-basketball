6abe2b3200c8c3a2ca0d71aff9f698d2c6faf36d
Hamza Warsame <hamza360123@gmail.com>
2022-01-14 17:33:04 -0500

    add Navbar component

--- client/basketball-app/src/App.js
+++ client/basketball-app/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import { Nav } from 'react-bootstrap';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Router>
       <Switch>
       <Route path="/" exact render={() => <Home/>}/>
       </Switch>
     </Router>
+    </>
   );
 }
 
