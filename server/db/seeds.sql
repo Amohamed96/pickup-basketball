e13aceaa7db12b737dbae499304b674d86bec4d1
Hamza Warsame <hamza360123@gmail.com>
2022-01-14 21:57:02 -0500

    remove create at end of db

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -5,4 +5,4 @@ VALUES
   ('Tanker', 'Tanker@gmail.com', '123456', 'https://i.imgur.com/T2WwVfS.png'),
   ('Awab', 'Awab@gmail.com', '123456', 'https://i.imgur.com/FK8V841.jpg'),
   ('Eddy', 'Eddy@gmail.com', '123456', 'https://i.imgur.com/twYrpay.jpg'),
-  ('Kobz', 'Kobz@gmail.com', '123456', 'https://i.imgur.com/TdOAdde.jpg');clear
+  ('Kobz', 'Kobz@gmail.com', '123456', 'https://i.imgur.com/TdOAdde.jpg');
