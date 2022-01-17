428b30870bf8b26f43d8f48c0562b0addcc41287
Hamza Warsame <hamza360123@gmail.com>
2022-01-17 14:36:16 -0500

    socket features

--- client/basketball-app/src/components/Navbar.js
+++ client/basketball-app/src/components/Navbar.js
@@ -15,8 +15,11 @@ export default function Navbar() {
         <ul class="pt-5">
           <li><a href="/">Home</a></li>
           <li><a href="/profile">Profile</a></li>
+          <li><a href="/messages">Messages</a></li>
           <li><a href="/login">Login</a></li>
           <li><a href="/register">Register</a></li>
+         
+          
         </ul>
       </nav>
     </>
--- client/basketball-app/src/pages/Home/Home.js
+++ client/basketball-app/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './Home.css'
 import Hero from "../../components/Hero";
+import Navbar from "../../components/Navbar";
 
 
  export default function Home () {
   return (
     <>
+    <Navbar />
     <Hero />
     <div className="homepage">
       Hello World
