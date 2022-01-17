e4a01e50905f9626d59de8a66a871ae39713726c
Hamza Warsame <hamza360123@gmail.com>
2022-01-17 14:39:24 -0500

    server old changes for socket

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const {Server} = require('socket.io');
+const port = 3001;
+app.use(cors());
 
-const port = 3001
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+}) 
 
-app.listen(port, (req, res) => {
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected ${socket.id}`)
+  })
+}) 
+
+app.listen(port, () => {
   console.log('Server Running....');
 })
+
