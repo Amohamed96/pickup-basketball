a25871d498d8a9f4dbc4907c584a3c02bcd4324a
Hamza Warsame <hamza360123@gmail.com>
2022-01-18 17:50:02 -0500

    update db

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -6,11 +6,11 @@ INSERT INTO users (
   password, 
   avatar) 
 VALUES
-  (4, 'Hawarsa','Playmaker', 'Hawarsa@gmail.com', '123456', 'https://i.dlpng.com/static/png/5254854-allen-iverson-png-95-images-in-collection-page-2-allen-iverson-png-384_384_preview.png'),
-  (1, 'LB', 'GOAT', 'LB@gmail.com', '123456', 'https://i.imgur.com/Nmx0Qxo.png'),
+  (4, 'Hawarsa','Playmaker', 'Hawarsa@gmail.com', '123456', 'https://spng.pngfind.com/pngs/s/16-168710_russell-westbrook-png-transparent-png.png' ),
+  (1, 'LB', 'GOAT', 'LB@gmail.com', '123456', 'https://i.dlpng.com/static/png/5254854-allen-iverson-png-95-images-in-collection-page-2-allen-iverson-png-384_384_preview.png'),
   (1, 'Tanker', 'Spot up marksman', 'Tanker@gmail.com', '123456', 'https://w7.pngwing.com/pngs/742/822/png-transparent-basketball-jersey-t-shirt-freddie-tshirt-sport-team-thumbnail.png'),
   (3, 'Awab00', 'Floor General', 'Awab@gmail.com', '123456', 'https://png.pngitem.com/pimgs/s/157-1577962_carmelo-anthony-knicks-png-transparent-png.png'),
   (2, 'EddyHamilton', '3pt Specialist', 'Eddy@gmail.com', '123456', 'https://png.pngitem.com/pimgs/s/374-3745230_kevin-durant-png-free-download-kevin-durant-brooklyn.png'),
   (4, 'Kobz24', 'Mid-range assasin', 'Kobz@gmail.com', '123456', 'https://image.pngaaa.com/163/1466163-small.png'),
   (2, 'LebanEsp', 'Left had virtuoso', 'Lebz@gmail.com', '123456', 'https://image.pngaaa.com/785/815785-small.png'),
-  (3, 'RexsIceCream', '3 and D', 'Rex@gmail.com', '123456', 'https://e7.pngegg.com/pngimages/682/318/png-clipart-stephen-curry-stephen-curry-dribbling-left-celebrities-sports-celebrities-thumbnail.png');;
+  (3, 'RexsIceCream', '3 and D', 'Rex@gmail.com', '123456', 'https://e7.pngegg.com/pngimages/682/318/png-clipart-stephen-curry-stephen-curry-dribbling-left-celebrities-sports-celebrities-thumbnail.png');
