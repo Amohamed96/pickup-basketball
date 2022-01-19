f2538c43669261ce87793577b4f10f38e792a835
Amohamed96 <amohamed.rg@gmail.com>
2022-01-19 11:51:12 -0500

    Leaderboad Jopepage

--- client/basketball-app/src/pages/Home/Home.js
+++ client/basketball-app/src/pages/Home/Home.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 import './Home.css'
 import Hero from "../../components/Hero";
 import Navbar from "../../components/Navbar";
+import Leaderboard from "../../components/Leaderboard";
 
- export default function Home () {
+export default function Home () {
 
   const [users, setUsers] = useState([])
   const [matches, setMatches] = useState([])
@@ -31,26 +32,28 @@ import Navbar from "../../components/Navbar";
     })
   }, [])
   
- console.log("USERS------>", users)
   return (
-    <>
-    <Navbar />
-    <Hero />
-  <div className="homepage">
-      Matches played: 
-        {matches.length > 0 ? matches.map((matchList) => {
-        return <ul> {matchList.date}{matchList.team1_score}{matchList.team2_score}</ul>
-      }): ""}
-      Teams: 
-        {teams.length > 0 ? teams.map((teamList) => {
-        return <ul><img className="teamLogo" src={teamList.avatar}/>{teamList.team_name}</ul>
-      }): ""}
-      Leaderboard: 
-        {users.length > 0 ? users.map((userList) => {
-        return <ul><img className="profilePic" src={userList.avatar}/>{userList.name}</ul>
-      }): ""}
-    </div>
-    
-    </> 
+      <>
+        <Navbar />
+        <Hero />
+        <Leaderboard/>
+        
+      <div className="homepage">
+          Matches played: 
+            {matches.length > 0 ? matches.map((matchList) => {
+            return <ul> {matchList.date}{matchList.team1_score}{matchList.team2_score}</ul>
+          }): ""}
+          Teams: 
+            {teams.length > 0 ? teams.map((teamList) => {
+            return <ul><img className="teamLogo" src={teamList.avatar}/>{teamList.team_name}</ul>
+          }): ""}
+          Leaderboard: 
+            {users.length > 0 ? users.map((userList) => {
+            return <ul><img className="profilePic" src={userList.avatar}/>{userList.name}</ul>
+          }): ""}
+
+      </div>
+                  
+  </>
   );
 }
