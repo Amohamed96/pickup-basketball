c54abf18a88f5db61e5dda0b2b071a4cb63834be
Amohamed96 <amohamed.rg@gmail.com>
2022-01-19 21:55:08 -0500

    Games updates

--- client/basketball-app/src/components/Games.js
+++ client/basketball-app/src/components/Games.js
@@ -4,15 +4,15 @@ import './Styles/Games.scss'
 export default function Games(props) {
   const {matches, teams} = props;
 
-  console.log("matches GAMES: ", matches)
-  console.log("teams GAMES: ", teams)
+
 
 const arrayMatches = Object.keys(matches).map(match => {
   const teamOneID = matches[match].team1_id
   const teamTwoID = matches[match].team2_id
-
+  console.log("MATCHES.DATE------>", matches[0].date)
   return (
     <div class="slide">
+      {matches[teamOneID].date}
       <img src={teams[teamOneID-1].avatar} height="100" width="250" alt="" />
       <img src={teams[teamTwoID-1].avatar} height="100" width="250" alt="" />
 		</div>
