4d1cdbbfd22fc31862b24180665237205721f71e
Hamza Warsame <hamza360123@gmail.com>
2022-01-21 00:32:55 -0500

    install packages and set up chat engine

--- client/basketball-app/src/App.js
+++ client/basketball-app/src/App.js
@@ -1,29 +1,26 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Home from './pages/Home/Home.js';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register.js';
-import Profile from './pages/Profile/Profile.js';
-import Navbar from './components/Navbar';
-import Messages from './pages/messages/Messages';
-import Player from './components/Player';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register.js";
+import Profile from "./pages/Profile/Profile.js";
+import Navbar from "./components/Navbar";
+import Messages from "./pages/messages/Messages";
+import Player from "./components/Player";
 
 function App() {
   return (
     <>
-    <Router>
-      <Switch>
-      <Route path="/" exact render={() => <Home/>}/>
-      <Route path="/register" exact render={() => <Register/>}/>
-      <Route path="/login" exact render={() => <Login/>}/>
-      <Route path="/profile" exact render={() => <Profile/>}/>
-      <Route path="/player/:id" render={() => <Player/>}/>
-      <Route path="/messages" exact render={() => <Messages/>}/>
-      </Switch>
-      
-    </Router>
-  
+      <Router>
+        <Switch>
+          <Route path="/" exact render={() => <Home />} />
+          <Route path="/register" exact render={() => <Register />} />
+          <Route path="/login" exact render={() => <Login />} />
+          <Route path="/profile" exact render={() => <Profile />} />
+          <Route path="/player/:id" render={() => <Player />} />
+          <Route path="/messages" render={() => <Messages />} />
+        </Switch>
+      </Router>
     </>
   );
 }
--- client/basketball-app/src/pages/messages/Messages.js
+++ client/basketball-app/src/pages/messages/Messages.js
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { ChatEngine } from "react-chat-engine";
 import "./Messages.css";
-import Navbar from "../../components/Navbar"; 
+import Navbar from "../../components/Navbar";
 
-export default function Messages() {
-  const [user, setUser] = useState({})
-  
-  useEffect(() => {
-    const currentUser = localStorage.getItem('user');
-    setUser(JSON.parse(currentUser))
-  }, [])
+function Messages() {
+  // const [user, setUser] = useState({});
+
+  // useEffect(() => {
+  //   const currentUser = localStorage.getItem("user");
+  //   setUser(JSON.parse(currentUser));
+  // }, []);
 
   return (
-    <>
-    <Navbar users={user}/>
-      <div className="messages">
-      <div className="chatbox">Chat</div>
-      <div className="menu"> Welcome {user.name}, I bet you're wondering who's online? </div>
-      </div>
-    </> 
+    <ChatEngine
+      height="100vh"
+      projectID="28515e67-5211-4871-82f6-cc1e9f8dfc9a"
+      userName="3ptChamp"
+      userSecret="12345"
+    />
   );
 }
+export default Messages;
