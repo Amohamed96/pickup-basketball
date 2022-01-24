eba39403d60b9ea0e1a0592cfefbe7f72d55d022
Hamza Warsame <hamza360123@gmail.com>
2022-01-23 22:06:40 -0500

    Challenges_recieved

--- client/basketball-app/src/components/ChallengesRecieved.js
+++ client/basketball-app/src/components/ChallengesRecieved.js
@@ -1,11 +1,47 @@
 import React, { useState, useEffect } from "react";
+import { Button, Card, Image } from "semantic-ui-react";
 import axios from "axios";
+import { CardGroup } from "react-bootstrap";
+
+const ButtonActions = (props) => {
+  return (
+    <Card.Content extra>
+      <div className="ui two buttons">
+        <Button
+          basic
+          color="green"
+          onClick={() => props.handleStatus(props.challenge, true)}
+        >
+          Approve
+        </Button>
+        <Button
+          basic
+          color="red"
+          onClick={() => props.handleStatus(props.challenge, false)}
+        >
+          Decline
+        </Button>
+      </div>
+    </Card.Content>
+  );
+};
+
 export default function ChallengesRecieved(props) {
   const { users, challenges } = props;
   const [error, setError] = useState("");
   const [user, setUser] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+  const [response, setResponse] = useState(false);
   const currentUser = localStorage.getItem("user");
+  let response_message = "";
   console.log("USERS *********--", users);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/users").then((res) => {
+      setAllUsers(res.data);
+      console.log("HOME RES DATA----->", res.data);
+    });
+  });
 
   const handleStatus = (challenge, newStatus) => {
     console.log("CHALLENGE *********--", challenge);
@@ -25,6 +61,12 @@ export default function ChallengesRecieved(props) {
       .then((result) => {
         // localStorage.setItem("user", JSON.stringify(result.data));
         // setRedirect("/profile");
+        setResponse(true);
+        if (newStatus) {
+          response_message = "You have accepted";
+        } else {
+          response_message = "You have declined";
+        }
       })
 
       .catch((err) => {
@@ -32,12 +74,40 @@ export default function ChallengesRecieved(props) {
         setError("ERROR");
       });
   };
+
   console.log("USERS CH===>", users);
-  console.log("USERS CH===>", users);
+  console.log("USERS CHALLENGES!===>", challenges);
 
   return (
     <>
-      <div>{/* {users[1].name} */}</div>
+      {challenges.map((challenge) => {
+        return (
+          <Card.Group>
+            <Card>
+              <Card.Content>
+                <Image
+                  floated="right"
+                  size="mini"
+                  src={allUsers[challenge.challenger_id].avatar}
+                />
+                <Card.Header>Name goes here</Card.Header>
+                <Card.Meta>Location goes here</Card.Meta>
+                <Card.Description>
+                  <strong>{challenge.challenge_message}</strong>
+                </Card.Description>
+              </Card.Content>
+              {challenge.request_status && (
+                <ButtonActions
+                  challenge={challenge}
+                  handleStatus={handleStatus}
+                />
+              )}
+            </Card>
+          </Card.Group>
+        );
+      })}
+
+      {/* <div>{users[1].name}</div>
       <div className="leaderboardContainer">
         <section class="content-info">
           <div class="container paddings-mini">
@@ -55,6 +125,7 @@ export default function ChallengesRecieved(props) {
                       </tr>
                     </thead>
                     <tbody class="text-center">
+                      `{" "}
                       <tr>
                         <td class="text-left number">
                           1<i class="fa fa-caret-up" aria-hidden="true"></i>
@@ -65,7 +136,7 @@ export default function ChallengesRecieved(props) {
                         </td>
                         <td>{challenge.challenge_message}</td>
                         <td>
-                          <button onClick={() => handleStatus(challenge, true)}>
+                          <button onClick={() => }>
                             ACCEPT
                           </button>
                         </td>
@@ -76,6 +147,7 @@ export default function ChallengesRecieved(props) {
                             DECLINE
                           </button>
                         </td>
+                        <p>{response_message}</p>
                         <td></td>
                       </tr>
                     </tbody>
@@ -86,6 +158,7 @@ export default function ChallengesRecieved(props) {
           </div>
         </section>
       </div>
+                ) */}
     </>
   );
 }
--- client/basketball-app/src/pages/messages/Messages.js
+++ client/basketball-app/src/pages/messages/Messages.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ChatEngine, ChatFeed } from "react-chat-engine";
 import "./Messages.css";
 import Chatfeed from "../../components/MessageComponents/Chatfeed";
-
+import MenuExampleInvertedSegment from "../../components/Menu/Menu";
 export default function Messages() {
   const [message, setMessage] = useState([]);
   const [user, setUser] = useState({});
@@ -13,13 +13,18 @@ export default function Messages() {
     console.log(" USER from LOCAL", user);
   }, [currentUser]);
   return (
-    <ChatEngine
-      height="100vh"
-      projectID="3fbd0f91-4a93-4897-8e55-06d72646a735"
-      userName={JSON.parse(currentUser).name}
-      userSecret={JSON.parse(currentUser).secret}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={(chatId, message) => console.log("DATA>", chatId, message)}
-    />
+    <>
+      <MenuExampleInvertedSegment />
+      <ChatEngine
+        height="100vh"
+        projectID="3fbd0f91-4a93-4897-8e55-06d72646a735"
+        userName={JSON.parse(currentUser).name}
+        userSecret={JSON.parse(currentUser).secret}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={(chatId, message) =>
+          console.log("DATA>", chatId, message)
+        }
+      />
+    </>
   );
 }
