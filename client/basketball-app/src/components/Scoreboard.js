00868959117f206f5d051d5136fa9a040e4fc338
Amohamed96 <amohamed.rg@gmail.com>
2022-01-26 16:43:44 -0500

    Home and player updates

--- client/basketball-app/src/components/Scoreboard.js
+++ client/basketball-app/src/components/Scoreboard.js
@@ -1,15 +1,14 @@
 // import React, { useState } from "react";
 
-// export default function Scoreboard() {
-//   let [homeScore, setHomeScore] = useState(0);
-//   const homeScoreTwo = function () {
-//     setHomeScore(homeScore + 2);
-//   };
+// const [count, setCount] = useState(0);
+// export const Scoreboard = () => {
 //   return (
-//     <div>
-//       {homeScore}
-//       <button onClick={homeScorePlus}>Home +2</button>
-//       <button onClick={homeScoreMinus}>Home +2</button>
+//     <div className="score-container">
+//       <div>{count}</div>
+//       <div className="button-wrapper">
+//         <button onClick=>-</button>
+//         <button>+</button>
+//       </div>
 //     </div>
 //   );
-// }
+// };
