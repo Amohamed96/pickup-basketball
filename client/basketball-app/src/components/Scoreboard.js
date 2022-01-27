4d7863e9c2e2a158d5b427a78092cb2365c43342
Amohamed96 <amohamed.rg@gmail.com>
2022-01-26 19:37:02 -0500

    Scoreboard

--- client/basketball-app/src/components/Scoreboard.js
+++ client/basketball-app/src/components/Scoreboard.js
@@ -1,14 +1,54 @@
 // import React, { useState } from "react";
-
-// const [count, setCount] = useState(0);
+// import "./Styles/Scoreboard.scss";
+// import Timer from "./Timer";
 // export const Scoreboard = () => {
+//   const [countOne, setCountOne] = useState(0);
+//   const [countTwo, setCountTwo] = useState(0);
+
 //   return (
-//     <div className="score-container">
-//       <div>{count}</div>
-//       <div className="button-wrapper">
-//         <button onClick=>-</button>
-//         <button>+</button>
+//     <div className="scoreboard-container">
+//       <div className="timer-container">
+//         <Timer />
 //       </div>
+//       <div className="score-container">
+//         <div className="team-container">
+//           <div className="count-score">{countOne}</div>
+//           <div className="button-wrapper">
+//             <button onClick={() => setCountOne(countOne - 1)}>-</button>
+//             <button onClick={() => setCountOne(countOne + 1)}>+</button>
+//             <button onClick={() => setCountOne(countOne + 2)}>+2</button>
+//           </div>
+//           <input
+//             name="player1_id"
+//             placeholder="Player 1"
+//             className="player-input"
+//           ></input>
+//         </div>
+//         <div className="team-container">
+//           <div className="count-score">{countTwo}</div>
+//           <div className="button-wrapper">
+//             <button onClick={() => setCountTwo(countTwo - 1)}>-</button>
+//             <button onClick={() => setCountTwo(countTwo + 1)}>+</button>
+//             <button onClick={() => setCountTwo(countTwo + 2)}>+2</button>
+//           </div>
+//           <input
+//             name="player2_id"
+//             placeholder="Player 2"
+//             className="player-input"
+//           ></input>
+//         </div>
+//       </div>
+//       <input
+//         name="winner_id"
+//         className="player-input"
+//         placeholder="Winner"
+//       ></input>
+//       <input
+//         type="submit"
+//         className="button"
+//         value="SUBMIT"
+//         onClick={handleMatchesPlayer}
+//       ></input>
 //     </div>
 //   );
 // };
